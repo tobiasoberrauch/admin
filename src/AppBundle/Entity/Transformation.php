c4c76b21d705eb1a4f6f59c7fe7425bcc092d2b1
Tobias Oberrauch <t.oberrauch@simplicity.ag>
2017-10-22 13:55:45 +0200

    Before structure

---
+++ src/AppBundle/Entity/Transformation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Class Transformation
+ *
+ * PHP Version 7
+ *
+ * @category  PHP
+ * @package   AppBundle\Entity
+ * @author    Simplicity Trade GmbH <development@simplicity.ag>
+ * @copyright 2014-2017 Simplicity Trade GmbH
+ * @license   Proprietary http://www.simplicity.ag
+ */
+class Transformation
+{
+}
